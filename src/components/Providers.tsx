70535e949ec28d7f4ae7407db85a7f08da80523f
Skaps664 <msudaisk664@gmail.com>
2024-11-23 14:22:08 +0500

    stripe success and error url updated

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://localhost:3000/stripe/success"
-      cancelUrl="https://localhost:3000/stripe/error"
+      successUrl="https://www.haarsheonline.net/stripe/success"
+      cancelUrl="https://www.haarsheonline.net/error"
       currency="USD"
       billingAddressCollection={true}
       shouldPersist={true}
